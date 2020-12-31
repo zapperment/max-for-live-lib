3a3bb5912314128063a8867a8d3aea0fb282a384
Patrick Hund <pahund@team.mobile.de>
2020-12-31 10:41:48 +0100

    * refactored directory structure, one package per object * adjusted webpack build and MaxForLivePlugin to support multiple objects * added new object pickUp
    Change-Id: I5348ea316be1487fbf8c945c227bafcb4b76334a

---
+++ src/objects/pickUp/pickUp.ts
@@ -0,0 +1,9 @@
+import { log } from "../../util";
+
+export const autowatch = 1;
+export const inlets = 1;
+export const outlets = 1;
+
+export function bang() {
+  log("Bang bang, he shot me down!")
+}
--- src/webpack/MaxForLivePlugin.js
+++ src/webpack/MaxForLivePlugin.js
@@ -2,52 +2,55 @@ const { ConcatSource } = require("webpack-sources");
 const { Compilation } = require("webpack");
 
 class MaxForLivePlugin {
-    constructor({ objectName }) {
-        this.objectName = objectName;
-    }
-    apply(compiler) {
-        compiler.hooks.compilation.tap("BannerPlugin", compilation => {
-			compilation.hooks.processAssets.tap({
-                name: "MaxForLivePlugin",
-                stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONS
-            },
-            () => {
-                for (const chunk of compilation.chunks) {
-                    // only add to initial chunk
-                    if (!chunk.canBeInitial()) {
-                        continue;
-                    }
-                    for (const file of chunk.files) {
-                        compilation.updateAsset(
-                            file,
-                            old => new ConcatSource(old, "\n" + `
+  apply(compiler) {
+    compiler.hooks.compilation.tap("BannerPlugin", (compilation) => {
+      compilation.hooks.processAssets.tap(
+        {
+          name: "MaxForLivePlugin",
+          stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONS,
+        },
+        () => {
+          for (const chunk of compilation.chunks) {
+            // only add to initial chunk
+            if (!chunk.canBeInitial()) {
+              continue;
+            }
+            for (const file of chunk.files) {
+              compilation.updateAsset(
+                file,
+                (old) =>
+                  new ConcatSource(
+                    old,
+                    "\n" +
+                      `
 
-autowatch = ${this.objectName}.autowatch || 0;
-inlets = ${this.objectName}.inlets || 1;
-outlets = ${this.objectName}.outlets || 1;
+autowatch = ${chunk.name}.autowatch || 0;
+inlets = ${chunk.name}.inlets || 1;
+outlets = ${chunk.name}.outlets || 1;
 
 function bang() {
-    if (typeof ${this.objectName}.bang !== 'function') {
+    if (typeof ${chunk.name}.bang !== 'function') {
         return;
     }
-    return ${this.objectName}.bang.apply(null, arguments);
+    return ${chunk.name}.bang.apply(null, arguments);
 }
 
 function anything() {
-    if (typeof ${this.objectName}.anything !== 'function') {
+    if (typeof ${chunk.name}.anything !== 'function') {
         return;
     }
-    return ${this.objectName}.anything.apply(null, arguments);
+    return ${chunk.name}.anything.apply(null, arguments);
 }
 
-                                `.trim())
-
-                        );
-                    }
-                }
-            })
-        })
-    }
+                                `.trim()
+                  )
+              );
+            }
+          }
+        }
+      );
+    });
+  }
 }
 
 module.exports = MaxForLivePlugin;
