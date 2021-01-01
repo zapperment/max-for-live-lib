5556abcd51acc7f5c16e4f65f245a38f9637b9db
Patrick Hund <pahund@team.mobile.de>
2021-01-01 11:52:36 +0100

    …squash me…
    Change-Id: I4e6d3a149c0b47c97be32c3cdee202b4c2a2df91

--- src/objects/pickUp/pickUp.ts
+++ src/objects/pickUp/pickUp.ts
@@ -1,5 +1,3 @@
-import { log } from "../../util";
-
 export const autowatch = 1;
 export const inlets = 2;
 export const outlets = 1;
@@ -11,47 +9,34 @@ let isOpen = false;
 
 export function msg_int(value: number) {
   if (inlet === 1) {
-    log("inlet 2 received value", value);
-    log("pick up value set to", value);
     pickupValue = value;
     recentlySet = true;
     isOpen = false;
     return;
   }
-  log("inlet 1 received value", value);
   if (isOpen) {
-    log("pick up is already open");
-    log("sending value", value, "to outlet 1");
     outlet(0, value);
     return;
   }
   if (recentlySet) {
-    log("pick up was recently set");
     recentlySet = false;
     if (value < pickupValue) {
-      log("below pick up value, direction is up");
       direction = "up";
       return;
     }
     if (value > pickupValue) {
-      log("below pick up value, direction is down");
       direction = "down";
       return;
     }
-    log("value matches pickup value");
-    log("sending value", value, "to outlet 1");
     isOpen = true;
     outlet(0, value);
   }
   if (direction === "up" && value < pickupValue) {
-    log("value is below pick up value, pick up stays closed");
     return;
   }
   if (direction === "down" && value > pickupValue) {
-    log("value is above pick up value, pick up stays closed");
     return;
   }
-  log("pick up value reached, pick up is now open");
   isOpen = true;
   outlet(0, value);
 }
